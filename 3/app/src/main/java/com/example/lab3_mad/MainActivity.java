971485daaf84414a9f798b1aa12b2302067e0321
Miulesi Kulasekara <it21028878@my.sliit.lk>
2022-09-02 09:33:37 +0530

    Commit message1

--- 3/app/src/main/java/com/example/lab3_mad/MainActivity.java
+++ 3/app/src/main/java/com/example/lab3_mad/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lab3;
+package com.example.lab3_mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- 3/app/src/test/java/com/example/lab3_mad/ExampleUnitTest.java
+++ 3/app/src/test/java/com/example/lab3_mad/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.lab3;
+package com.example.lab3_mad;
 
 import org.junit.Test;
 
